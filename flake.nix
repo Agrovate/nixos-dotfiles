1242e6825ee7f7d6f5de99ba681aa2d082e64ba4
Your Name <you@example.com>
2026-05-01 14:22:27 +0530

    aah my brain

--- flake.nix
+++ flake.nix
@@ -4,52 +4,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    import-tree.url = "github:vic/import-tree";
+
+    wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hyprland.url = "github:hyprwm/Hyprland";
   };
-  outputs = {self, nixpkgs, home-manager, hyprland, ...} @ inputs:
-  let
-    system = "x86_64-linux";
-  in
-  {
-    nixosConfigurations.mac-vm = nixpkgs.lib.nixosSystem {
-      inherit system;
 
-      modules = [
-        ./hosts/mac-vm/configuration.nix
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-
-	  home-manager.users.snow = {
-	    home.stateVersion = "25.11";
-	  };
-	}
-      ];
-    };
-
-    nixosConfigurations.delly = nixpkgs.lib.nixosSystem {
-      inherit system;
-
-      specialArgs = { inherit inputs; };
-
-      modules = [
-        ./hosts/delly/configuration.nix
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-
-	  home-manager.users.snow = {
-        imports = [ ./modules/home/home.nix ];
-	    home.stateVersion = "25.11";
-	  };
-	}
-      ];
-    };
-  };
+  outputs = inputs: inputs.flake-parts.lib.mkFlake
+    {inherit inputs;}
+    (inputs.import-tree ./modules);
 }
---
+++ modules/hosts/delly/configuration.nix
@@ -0,0 +1,117 @@
+{self, inputs, ...}: {
+    flake.nixosModules.myMachineConfiguration = {pkgs, lib, ...}: {
+        imports = [
+            self.nixosModules.myMachineHardware
+            self.nixosModules.niri
+            inputs.home-manager.nixosModules.home-manager
+        ];
+
+
+        boot.loader = {
+            grub = {
+                enable = true;
+                efiSupport = true;
+                device = "nodev";
+                useOSProber = false;
+                extraEntries = ''
+
+    menuentry 'Arch Linux (on /dev/nvme0n1p3)' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-simple-33600e80-6042-4719-8b32-ab221bdc417a' {
+        insmod part_gpt
+        insmod ext2
+        search --no-floppy --fs-uuid --set=root 33600e80-6042-4719-8b32-ab221bdc417a
+        linux /boot/vmlinuz-linux root=UUID=33600e80-6042-4719-8b32-ab221bdc417a rw loglevel=3 quiet
+        initrd /boot/initramfs-linux.img
+    }
+                '';
+            };
+            efi = {
+                canTouchEfiVariables = true;
+                efiSysMountPoint = "/boot";
+            };
+        };
+
+        boot.kernelPackages = pkgs.linuxPackages_latest;
+
+        services.dbus.enable = true;
+        xdg.portal.enable= true;
+        xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+
+
+        networking.networkmanager.enable = true;
+
+        time.timeZone = "Asia/Kolkata";
+
+
+        services.xserver.enable = false;
+
+
+        services.pipewire = {
+            enable = true;
+            pulse.enable = true;
+        };
+
+        programs.firefox.enable = true;
+        programs.zsh.enable = true;
+        programs.starship.enable = true;
+
+
+        programs.direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+            enableZshIntegration = true;
+            settings = {
+                global = {
+                    hide_env_diff = true;
+                };
+            };
+        };
+
+        environment.systemPackages = with pkgs; [
+            vim
+            wget
+            git
+            neovim
+            ghostty
+            wl-clipboard
+            stow
+            hyprpaper
+            tofi
+            zoxide
+            eza
+            gcc
+        ];
+
+        users.users.snow = {
+            isNormalUser = true;
+            extraGroups = [ "wheel" ];
+            initialPassword = "isha";
+            shell = pkgs.zsh;
+        };
+
+        services.greetd = {
+            enable = true;
+            settings = {
+                default_session = {
+                    command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+                    user = "greeter";
+                };
+            };
+        };
+        systemd.services.greetd.serviceConfig = {
+            Type = "idle";
+            StandardInput = "tty";
+            StandardOutput = "null";
+            StandardError = "journal";
+            TTYReset = true;
+            TTYVHangup = true;
+            TTYVTDisallocate = true;
+        };
+
+        services.openssh = {
+            enable= true;
+        };
+
+        nix.settings.experimental-features = ["nix-command" "flakes" ];
+        system.stateVersion = "25.11";
+    };
+}
